57367b7f517049a918396773d23931cb96b90559
Dallin parker <13424017+dallin-r-parker@users.noreply.github.com>
2019-10-17 19:53:13 -0400

    basic news app structure

---
+++ README.md
@@ -0,0 +1,6 @@
+## PWA 
+
+the amount of data available = navigator.storage.estamate()
+
+## api key 
+200d811316a34fe68d038e487bc3db78
---
+++ index.js
@@ -0,0 +1,35 @@
+const API_KEY = "200d811316a34fe68d038e487bc3db78";
+
+async function getNews() {
+  const response = await fetch(
+    `https://newsapi.org/v2/everything?q=bitcoin&from=2019-09-17&sortBy=publishedAt&apiKey=${API_KEY}`
+  );
+  const results = await response.json();
+  renderNews({ news: results.articles });
+}
+
+function renderCard({ title, imgLink, date }) {
+  /*html*/
+  return `
+    <div class="card">
+      <img src=${imgLink} alt="" class="card-img-top"/>
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text"><small class="text-muted">${date}</small></p>
+      </div>
+    </div>
+  `;
+}
+
+function renderNews({ news }) {
+  const cols = document.getElementById("cardContainer");
+  news.map(article => {
+    return (cols.innerHTML += renderCard({
+      title: article.title,
+      imgLink: article.urlToImage,
+      date: new Date(article.publishedAt)
+    }));
+  });
+}
+
+getNews();

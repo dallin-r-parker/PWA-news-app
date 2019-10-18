5f3983482ea5e572c741a98e30e241babc8be980
Dallin parker <13424017+dallin-r-parker@users.noreply.github.com>
2019-10-17 20:48:05 -0400

    added vanilla js service worker logic

--- sw.js
+++ sw.js
@@ -1,1 +1,26 @@
-console.log(`i am the service worker: `);
+const CACHE_NAME = "CACHE_NAME_V1";
+
+const CACHE_FILES = ["./", "/index.html", "/index.js", "/style.css"];
+
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_FILES))
+  );
+});
+
+self.addEventListener("fetch", function(evt) {
+  evt.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(resp => {
+        return (
+          resp ||
+          fetch(evt.request).then(response => {
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+          })
+        );
+      });
+    })
+  );
+});
